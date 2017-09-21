c313c1446688bc1e62c2c8aef284676ba04a20f2
KarenF <karenf@outlook.com>
2017-09-21 15:31:24 -0300

    arruma exercicio

--- credit_cards.js
+++ credit_cards.js
@@ -1,38 +1,31 @@
-let card_number  = ['5','1','1','1','3','4','5','5','5','5','5','5','5','5','5','5'];
-let flag;
+let card_number = '4408041234567893';
 
+cardArray = card_number.split('');
+inverseCard = cardArray.reverse();
 
-
-if((card_number.slice(0,2) == "34" || card_number.slice(0,2) == "37") && card_number.length == 15){
-  console.log("FLAG: AMEX");
-  flag = "AMEX";
-}else if ((card_number.slice(0,2) == "51" || card_number.slice(0,2) == "55") && card_number.length == 16){
-  console.log("FLAG: Master Card");
-  flag = "MASTER";
-}else if ((card_number.slice(0,4) == "6011") && card_number.length == 16){
-  console.log("FLAG: Discover");
-  flag = "DISCOVER";
-}else if ((card_number.slice(0,1) == "4") && card_number.length == 13 || card_number.length == 16){
-  console.log("FLAG: Visa");
-  flag = "VISA";
-}else{
-  console.log("Credit Card Invalid.")
+var sumNumbers = 0;
+for(let i = 0; i < inverseCard.length; i++){
+  if(i % 2 != 0){
+    inverseCard[i] = inverseCard[i] * 2;
+  }
+  if(inverseCard[i] > 9){
+    inverseCard[i] = inverseCard[i] - 9;
+  }
+  sumNumbers += parseInt(inverseCard[i]);
 }
 
-// card_number.forEach(function())
-
-
- for (let i ; i < card_number.length; i++){
-    var aux = card_number[i];
-   				card_number[i] = card_number[i+1];
-   				card_number[i+1] = aux;
-
- }
-
-card_number.reverse();
-
-var numbercard = card_number.join('')
-
-var testuda = parseInt(card_number);
-
- console.log("TESTE",numbercard, " int >  ", testuda);
+if(sumNumbers % 10 == 0){
+  if((card_number.slice(0,2) == "34" || card_number.slice(0,2) == "37") && card_number.length == 15){
+    console.log("FLAG: AMEX");
+  }else if ((card_number.slice(0,2) >= "51" && card_number.slice(0,2) <= "55") && card_number.length == 16){
+    console.log("FLAG: Master Card");
+  }else if ((card_number.slice(0,4) == "6011") && card_number.length == 16){
+    console.log("FLAG: Discover");
+  }else if((card_number.slice(0,1) == "4") && (card_number.length == 13 || card_number.length == 16)){
+    console.log("FLAG: Visa");
+  }else{
+    console.log("Flag not identified");
+  }
+}else{
+  console.log("Credit Card Invalid.");
+}
